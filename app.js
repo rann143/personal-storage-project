d3f0c3905a48aa2a07ba6791fda77040e479882d
Daniel <roderm99@gmail.com>
2024-09-04 10:17:30 -0400

    added necessary dependencies for project. configure sessions with express-session and prisma session store

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
+const passport = require("passport");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    // eslint-disable-next-line no-undef
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
