094ba8f5511ce13af2091ebfb9e70ded45d4dbbc
Daniel <roderm99@gmail.com>
2024-10-28 14:36:28 -0400

    add authenication check for all required routes - abstract functionality into its own file

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const passport = require("passport");
 const userController = require("../controllers/user-controller");
 const folderController = require("../controllers/folder-controller");
 const fileController = require("../controllers/file-controller");
+const checkAuthenticated =
+  require("../auth-config/authMiddleware").checkAuthenticated;
 const multer = require("multer");
 const upload = multer({
   dest: "uploads/",
@@ -49,32 +51,23 @@ router.get("/signup", function (req, res) {
 router.post("/signup", userController.user_create_post);
 
 // HOME ROUTE
-router.get(
-  "/home",
-  function (req, res, next) {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.send("You are not authenticated");
-    }
-  },
-  folderController.get_all_folders,
-);
+router.get("/home", checkAuthenticated, folderController.get_all_folders);
 
 // UPLOAD FILE ROUTE (GET)
-router.get("/home/:folder/:folderId/upload-file", function (req, res) {
-  if (req.isAuthenticated()) {
+router.get(
+  "/home/:folder/:folderId/upload-file",
+  checkAuthenticated,
+  function (req, res) {
     res.render("uploadfile-form", {
       uploadSuccessful: "",
     });
-  } else {
-    res.send("You are not authenticated");
-  }
-});
+  },
+);
 
 // UPLOAD FILE ROUTE (POST)
 router.post(
   "/home/:folder/:folderId/upload-file",
+  checkAuthenticated,
   upload.single("uploaded_file"),
   fileController.upload_file_to_folder_post,
 );
@@ -85,47 +78,50 @@ router.post("/home/:folder?/:folderId?", folderController.add_folder_post);
 // SELECTED FOLDER ROUTE (GET)
 router.get(
   "/home/:folder?/:folderId?",
-  function (req, res, next) {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.send("You are not authenticated");
-    }
-  },
+  checkAuthenticated,
   folderController.selected_folder_get,
 );
 
 // UPDATE FOLDER FORM PAGE (GET)
 router.get(
   "/home/:folder/:folderId?/update",
+  checkAuthenticated,
   folderController.selected_folder_update_get,
 );
 
 // UPDATE FOLDER FORM PAGE (POST)
 router.post(
   "/home/:folder/:folderId?/update",
+  checkAuthenticated,
   folderController.selected_folder_update_put,
 );
 
 // DELETE FOLDER ROUTE
 router.post(
   "/home/:folder/:folderId?/delete",
+  checkAuthenticated,
   folderController.selected_folder_delete_post,
 );
 
 // FILE DETAIL ROUTE (GET)
 router.get(
   "/home/:folder/:folderId?/:fileName?/details",
+  checkAuthenticated,
   fileController.file_detail,
 );
 
 // FILE DETAIL PAGE (DELETE POST)
 router.post(
   "/home/:folder/:folderId?/:fileName?/details",
+  checkAuthenticated,
   fileController.file_delete,
 );
 
 // FILE DOWNLOAD ROUTE (GET)
-router.get("/download/:fileName", fileController.download_file_get);
+router.get(
+  "/download/:fileName",
+  checkAuthenticated,
+  fileController.download_file_get,
+);
 
 module.exports = router;
